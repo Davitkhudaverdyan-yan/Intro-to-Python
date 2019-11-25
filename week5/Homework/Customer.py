65deb4307ecc3f4b48c91bb5b0cd603c4185b977
davitkhudaverdyan-yan <davit.khudaverdyan@gmail.com>
2019-11-25 21:08:58 +0400

    add homework week 5

---
+++ week5/Homework/Customer.py
@@ -0,0 +1,12 @@
+from Productcheck import products
+
+def check(product,num,price):
+    if product.check(product,num)== True:
+        print("You bought " + product + " and spent "  num *price)
+    else:
+        print("Sorry! We are out of this product.")
+
+def main("candy", 9, 9)
+
+if __name__ == '__main__':
+    main()
